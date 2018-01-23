1dff516e54cdde10de5523d957c2625a8c89aae9
LeonXi <cdxiyang@gmail.com>
2018-01-23 10:45:14 +0800

    add spring-unit; refresh Article VO

--- kuangkee-manager-mapper/src/test/java/com/kuangkee/mybatis/test/MBTableGenerated.java
+++ kuangkee-manager-mapper/src/test/java/com/kuangkee/mybatis/test/MBTableGenerated.java
@@ -15,14 +15,6 @@ import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
-//import com.atguigu.mybatis.bean.Employee;
-//import com.atguigu.mybatis.bean.EmployeeExample;
-//import com.atguigu.mybatis.bean.EmployeeExample.Criteria;
-//import com.atguigu.mybatis.dao.EmployeeMapper;
-
-/*import com.atguigu.mybatis.bean.Employee;
-import com.atguigu.mybatis.dao.EmployeeMapper;*/
-
 public class MBTableGenerated {
 
 	public SqlSessionFactory getSqlSessionFactory() throws IOException {
@@ -31,8 +23,13 @@ public class MBTableGenerated {
 		return new SqlSessionFactoryBuilder().build(inputStream);
 	}
 
+	/**
+	 * generateKuangKeeTables:生成前需先删除对应的XML,DO和Example. <br/>
+	 * @author Leon Xi
+	 * @throws Exception
+	 */
 	@Test
-	public void testMbg() throws Exception {
+	public void generateKuangKeeTables() throws Exception {
 		List<String> warnings = new ArrayList<String>();
 		boolean overwrite = true;
 		File configFile = new File("src/main/resources/mbg_kuangkee.xml");
@@ -43,54 +40,5 @@ public class MBTableGenerated {
 				callback, warnings);
 		myBatisGenerator.generate(null);
 	}
-	
-//	@Test
-//	public void testMyBatis3Simple() throws IOException{
-//		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
-//		SqlSession openSession = sqlSessionFactory.openSession();
-//		try{
-//			EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
-//			List<Employee> list = mapper.selectByExample(null);
-//			for (Employee employee : list) {
-//				System.out.println(employee.getId());
-//			}
-//		}finally{
-//			openSession.close();
-//		}
-//	}
-	
-//	@Test
-//	public void testMyBatis3() throws IOException{
-//		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
-//		SqlSession openSession = sqlSessionFactory.openSession();
-//		try{
-//			EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
-//			//xxxExample就是封装查询条件的
-//			//1、查询所有
-//			//List<Employee> emps = mapper.selectByExample(null);
-//			//2、查询员工名字中有e字母的，和员工性别是1的
-//			//封装员工查询条件的example
-//			EmployeeExample example = new EmployeeExample();
-//			//创建一个Criteria，这个Criteria就是拼装查询条件
-//			//select id, last_name, email, gender, d_id from tbl_employee 
-//			//WHERE ( last_name like ? and gender = ? ) or email like "%e%"
-//			Criteria criteria = example.createCriteria();
-//			criteria.andLastNameLike("%e%");
-//			criteria.andGenderEqualTo("1");
-//			
-//			Criteria criteria2 = example.createCriteria();
-//			criteria2.andEmailLike("%e%");
-//			example.or(criteria2);
-//			
-//			List<Employee> list = mapper.selectByExample(example);
-//			for (Employee employee : list) {
-//				System.out.println(employee.getId());
-//			}
-//			
-//		}finally{
-//			openSession.close();
-//		}
-//	}
-	
 
 }
